1c0dee57ffcb6e2f95a9a5f639afaa8d31b9e460
Brian Mielke <bbmielke@gmail.com>
2021-02-22 14:26:26 -0700

    Point to my module in terraform cloud modules

--- main.tf
+++ main.tf
@@ -1,9 +1,15 @@
+terraform {
+  required_providers = {
+    source = "hashicorp/aws"
+  }
+}
+
 provider "aws" {
   region = var.region
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
+  source  = "app.terraform.io/bbmielke/s3-webapp/aws"
   name        = var.name
   region = var.region
   prefix = var.prefix
